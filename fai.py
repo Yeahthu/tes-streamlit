04b5d9c39a8209a684c6d1c88d4a4f13c57d006a
Yeahthu <122600104+Yeahthu@users.noreply.github.com>
2024-07-16 10:50:14 +0000

    Update fai.py

--- fai.py
+++ fai.py
@@ -32,19 +32,8 @@ def streamlit_app():
             font-family: sans-serif;
             margin: 0;
             padding: 0;
-            background-color: #ffffff;
+            background-color: white;
         }
-        #Tampilan {
-            position: relative;
-            width: 100%;
-            margin: 10px auto;
-            background-color: #ffffff;
-            border-radius: 10px;
-            box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, 
-                        rgba(0, 0, 0, 0.12) 0px -12px 30px, 
-                        rgba(0, 0, 0, 0.12) 0px 4px 6px,
-                        rgba(0, 0, 0, 0.17) 0px 12px 13px, 
-                        rgba(0, 0, 0, 0.09) 0px -3px 5px;
         }
         .bagian-header {
             background-image: url("https://raw.githubusercontent.com/Yeahthu/tes-streamlit/main/bgHidroponik.jpg");
